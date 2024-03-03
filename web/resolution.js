a8b473232df1735dec08c118dd5357bb1ff49af1
Sven Op de Hipt <eisberg-zirkel-0c@icloud.com>
2024-03-03 16:05:36 +0100

    remove web support

--- web/resolution.js
+++
@@ -1,8 +0,0 @@
-if (screen.width < 450) {
-  document.body.classList.add('small_screen');
-  document.getElementsByClassName('html')[0].classList.add('small_screen');
-}
-else {
-  document.body.classList.add('large_screen');
-  document.getElementsByClassName('html')[0].classList.add('large_screen');
-}
