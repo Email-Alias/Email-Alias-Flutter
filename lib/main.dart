e3f225775b21f7bb285dcdf797c0fc1263e5f3f5
Sven Op de Hipt <TTnAf20PugxtDi.FOBmT@opdehipt.com>
2024-11-06 10:40:26 +0100

    add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,18 @@
+import 'dart:io';
 
 import 'package:email_alias/app/app.dart';
-import 'package:email_alias/app/config/config_controller.dart';
-import 'package:email_alias/app/database/database.dart';
-import 'package:email_alias/app/settings/settings_controller.dart';
+import 'package:email_alias/app/splash_screen.dart';
+import 'package:email_alias/initialization.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await _initializeDb();
-  await SettingsController.instance.initialize();
-  await ConfigController.instance.initialize();
-  LicenseRegistry.addLicense(() async* {
-    final materialIconsLicense = await rootBundle.loadString('licenses/MaterialIconsLicense');
-    yield LicenseEntryWithLineBreaks(
-      ['Material Icons'],
-      materialIconsLicense,
-    );
-  });
-  runApp(const AliasApp());
-}
-
-Future<void> _initializeDb() async {
-  emailDatabase = await $FloorAppDatabase.databaseBuilder('app_database.db').build();
+  if (kIsWeb || Platform.isAndroid || Platform.isIOS) {
+    WidgetsFlutterBinding.ensureInitialized();
+    await initializeApp();
+    runApp(const AliasApp());
+  }
+  else {
+    runApp(const SplashScreen());
+  }
 }
