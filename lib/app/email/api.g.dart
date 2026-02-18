244d703deed16a153c8232571189d09b7ffa9602
Sven Op de Hipt <TTnAf20PugxtDi.FOBmT@opdehipt.com>
2026-02-19 00:55:38 +0100

    update api to latest mailcow version

--- lib/app/email/api.g.dart
+++ lib/app/email/api.g.dart
@@ -10,6 +10,7 @@ _AddEmailRequest _$AddEmailRequestFromJson(Map<String, dynamic> json) =>
     _AddEmailRequest(
       active: json['active'] as bool,
       sogoVisible: json['sogo_visible'] as bool,
+      senderAllowed: json['sender_allowed'] as bool,
       address: json['address'] as String,
       goto: const StringToSetConverter().fromJson(json['goto'] as String),
       privateComment: json['private_comment'] as String,
@@ -19,6 +20,7 @@ Map<String, dynamic> _$AddEmailRequestToJson(_AddEmailRequest instance) =>
     <String, dynamic>{
       'active': instance.active,
       'sogo_visible': instance.sogoVisible,
+      'sender_allowed': instance.senderAllowed,
       'address': instance.address,
       'goto': const StringToSetConverter().toJson(instance.goto),
       'private_comment': instance.privateComment,
@@ -30,55 +32,53 @@ _AddEmailResponse _$AddEmailResponseFromJson(Map<String, dynamic> json) =>
     );
 
 Map<String, dynamic> _$AddEmailResponseToJson(_AddEmailResponse instance) =>
-    <String, dynamic>{
-      'msg': instance.msg,
-    };
+    <String, dynamic>{'msg': instance.msg};
 
 _UpdateEmailRequest _$UpdateEmailRequestFromJson(Map<String, dynamic> json) =>
     _UpdateEmailRequest(
       items: (json['items'] as List<dynamic>)
           .map((e) => (e as num).toInt())
           .toList(),
-      attr:
-          _UpdateEmailAttributes.fromJson(json['attr'] as Map<String, dynamic>),
+      attr: _UpdateEmailAttributes.fromJson(
+        json['attr'] as Map<String, dynamic>,
+      ),
     );
 
 Map<String, dynamic> _$UpdateEmailRequestToJson(_UpdateEmailRequest instance) =>
-    <String, dynamic>{
-      'items': instance.items,
-      'attr': instance.attr,
-    };
+    <String, dynamic>{'items': instance.items, 'attr': instance.attr};
 
 _UpdateEmailAttributes _$UpdateEmailAttributesFromJson(
-        Map<String, dynamic> json) =>
-    _UpdateEmailAttributes(
-      goto: _$JsonConverterFromJson<String, Set<String>>(
-          json['goto'], const StringToSetConverter().fromJson),
-      active: _$JsonConverterFromJson<int, bool>(
-          json['active'], const IntToBoolConverter().fromJson),
-    );
+  Map<String, dynamic> json,
+) => _UpdateEmailAttributes(
+  goto: _$JsonConverterFromJson<String, Set<String>>(
+    json['goto'],
+    const StringToSetConverter().fromJson,
+  ),
+  active: _$JsonConverterFromJson<int, bool>(
+    json['active'],
+    const IntToBoolConverter().fromJson,
+  ),
+);
 
 Map<String, dynamic> _$UpdateEmailAttributesToJson(
-        _UpdateEmailAttributes instance) =>
-    <String, dynamic>{
-      if (_$JsonConverterToJson<String, Set<String>>(
-              instance.goto, const StringToSetConverter().toJson)
-          case final value?)
-        'goto': value,
-      if (_$JsonConverterToJson<int, bool>(
-              instance.active, const IntToBoolConverter().toJson)
-          case final value?)
-        'active': value,
-    };
+  _UpdateEmailAttributes instance,
+) => <String, dynamic>{
+  'goto': ?_$JsonConverterToJson<String, Set<String>>(
+    instance.goto,
+    const StringToSetConverter().toJson,
+  ),
+  'active': ?_$JsonConverterToJson<int, bool>(
+    instance.active,
+    const IntToBoolConverter().toJson,
+  ),
+};
 
 Value? _$JsonConverterFromJson<Json, Value>(
   Object? json,
   Value? Function(Json json) fromJson,
-) =>
-    json == null ? null : fromJson(json as Json);
+) => json == null ? null : fromJson(json as Json);
 
 Json? _$JsonConverterToJson<Json, Value>(
   Value? value,
   Json? Function(Value value) toJson,
-) =>
-    value == null ? null : toJson(value);
+) => value == null ? null : toJson(value);
